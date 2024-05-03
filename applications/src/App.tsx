5d06c40faec4eabc27c938e028b6eebbeba3bad8
もっちゃん <s7056i3056@gmail.com>
2024-05-03 13:44:05 +0900

    move: componentsディレクトリの位置を間違えたので移動した

--- applications/src/App.tsx
+++ applications/src/App.tsx
@@ -1,8 +1,8 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "../../components/Counter";
-import { TodoList } from "../../components/TodoList";
-import { NotFound } from "../../components/NotFound";
-import { Home } from "../../components/Home";
+import { Counter } from "../components/Counter";
+import { TodoList } from "../components/TodoList";
+import { NotFound } from "../components/NotFound";
+import { Home } from "../components/Home";
 
 function App() {
   return (
