4fc2da4e8878bd626f92a2cbc1795db0647fd8e7
もっちゃん <s7056i3056@gmail.com>
2024-05-04 11:52:21 +0900

    feat: create increment and decrement button

--- applications/src/components/Counter/index.test.tsx
+++ applications/src/components/Counter/index.test.tsx
@@ -1,4 +1,37 @@
-test("counter add button test", () => {
-  const element = document.getElementById("button");
-  console.log(element);
+import { render } from "@testing-library/react";
+import { Counter } from ".";
+
+describe("Counter component test", () => {
+  test("p tag view test", () => {
+    const result = render(<Counter />);
+    expect(result.getByText("Welcome To Counter App")).toBeDefined();
+  });
+
+  test("Increment button view test", () => {
+    const result = render(<Counter />);
+    expect(result.getByText("Increment")).toBeDefined();
+  });
+
+  test("Increment button click test", () => {
+    const counter = render(<Counter />);
+    const button = counter.getByText("Increment");
+    button.click();
+    const result = render(<Counter />);
+    expect(result.getByText("1")).toBeDefined();
+  });
+
+  test("Decrement button view test", () => {
+    const result = render(<Counter />);
+    expect(result.getByText("Decrement")).toBeDefined();
+  });
+
+  test("Decrement button click test", () => {
+    const counter = render(<Counter />);
+    const incrementButton = counter.getByText("Increment");
+    incrementButton.click();
+    const decrementButton = counter.getByText("Decrement");
+    decrementButton.click();
+    const result = render(<Counter />);
+    expect(result.getByText("0")).toBeDefined();
+  });
 });
--- applications/src/components/Counter/index.tsx
+++ applications/src/components/Counter/index.tsx
@@ -2,10 +2,21 @@ import { useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+
+  function handleIncrement() {
+    setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    setCount(count - 1);
+  }
+
   return (
     <>
       <p>Welcome To Counter App</p>
-      <button>Add</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleDecrement}>Decrement</button>
+      <p>{count}</p>
     </>
   );
 };
