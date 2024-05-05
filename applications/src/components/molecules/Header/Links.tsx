f393237c68f47fac7372d102eaf1824240bcd0ff
もっちゃん <s7056i3056@gmail.com>
2024-05-05 15:00:00 +0900

    返り値の引数を追加

--- applications/src/components/molecules/Header/Links.tsx
+++ applications/src/components/molecules/Header/Links.tsx
@@ -2,7 +2,7 @@ import { CounterLink } from "../../atoms/Header/Link/CounterLink";
 import { HomeLink } from "../../atoms/Header/Link/HomeLink";
 import { TodoListLink } from "../../atoms/Header/Link/TodoListLink";
 
-export const Links = () => {
+export const Links = (): JSX.Element => {
   return (
     <>
       <HomeLink />
