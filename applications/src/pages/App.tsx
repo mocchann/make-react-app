52a15068dff21e74297d7aba98ae811195eaa55b
もっちゃん <s7056i3056@gmail.com>
2024-05-06 16:47:09 +0900

    refactor: pagesでtemplateを呼び出した

--- applications/src/pages/App.tsx
+++ applications/src/pages/App.tsx
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
-import { Link, Route, Routes } from "react-router-dom";
-import { Counter } from "../components/Counter";
-import { TodoList } from "../components/TodoList";
-import { NotFound } from "../components/NotFound";
-import { Home } from "../components/Home";
+import { Route, Routes } from "react-router-dom";
+import { CounterTemplate } from "../components/templates/Counter";
+import { TodoListTemplate } from "../components/templates/TodoList";
+import { NotFound } from "../components/templates/NotFound";
+import { HomeTemplate } from "../components/templates/Home";
 import { css } from "@emotion/react";
 
 function App() {
@@ -31,20 +31,10 @@ function App() {
 
   return (
     <div css={style}>
-      <h1>React Applications Menu</h1>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-      <p>
-        <Link to="/counter">Counter</Link>
-      </p>
-      <p>
-        <Link to="/todolist">TodoList</Link>
-      </p>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/counter" element={<Counter />} />
-        <Route path="/todolist" element={<TodoList />} />
+        <Route path="/" element={<HomeTemplate />} />
+        <Route path="/counter" element={<CounterTemplate />} />
+        <Route path="/todolist" element={<TodoListTemplate />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
