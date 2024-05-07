37f88451fa7a7de23e6c94a8934e7d56599d2f2b
もっちゃん <s7056i3056@gmail.com>
2024-05-07 23:01:01 +0900

    fix: useContextエラーになるためBrowserRouterでCounterTemplateをラップ

--- applications/src/components/templates/Counter/index.test.tsx
+++ applications/src/components/templates/Counter/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, RenderResult } from "@testing-library/react";
 import { CounterTemplate } from ".";
 import "@testing-library/jest-dom";
+import { BrowserRouter } from "react-router-dom";
 
 let counter: RenderResult;
 
@@ -10,19 +11,31 @@ describe("Counter component test", () => {
   });
 
   test("p tag view test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
 
     expect(counter.getByText("Welcome To Counter App")).toBeDefined();
   });
 
   test("Increment button view test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
 
     expect(counter.getByText("Increment")).toBeDefined();
   });
 
   test("Increment button click test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const button = await counter.getByText("Increment");
     await button.click();
 
@@ -30,13 +43,21 @@ describe("Counter component test", () => {
   });
 
   test("Decrement button view test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
 
     expect(counter.getByText("Decrement")).toBeDefined();
   });
 
   test("Decrement button click test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const incrementButton = await counter.getByText("Increment");
     await incrementButton.click();
     const decrementButton = await counter.getByText("Decrement");
@@ -46,7 +67,11 @@ describe("Counter component test", () => {
   });
 
   test("If the number is negative, the decrement button cannot be pressed.", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const decrementButton = await counter.getByText("Decrement");
     await decrementButton.click();
 
@@ -54,7 +79,11 @@ describe("Counter component test", () => {
   });
 
   test("If 0, the decrement button cannot be pressed", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const decrementButton = await counter.getByRole("button", {
       name: "Decrement",
     });
@@ -63,13 +92,21 @@ describe("Counter component test", () => {
   });
 
   test("Reset button view test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
 
     expect(counter.getByText("Reset")).toBeDefined();
   });
 
   test("Reset button click test", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const incrementButton = await counter.getByText("Increment");
     await incrementButton.click();
     const resetButton = await counter.getByText("Reset");
@@ -79,7 +116,11 @@ describe("Counter component test", () => {
   });
 
   test("If 0, the reset button cannot be pressed", async () => {
-    counter = await render(<CounterTemplate />);
+    counter = await render(
+      <BrowserRouter>
+        <CounterTemplate />
+      </BrowserRouter>
+    );
     const resetButton = await counter.getByRole("button", { name: "Reset" });
 
     expect(resetButton).toBeDisabled();
