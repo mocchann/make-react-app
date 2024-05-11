0d96d1ad904de848636abdeaf5b8c8039971bfab
もっちゃん <s7056i3056@gmail.com>
2024-05-11 12:57:03 +0900

    feat: 最高気温と最低気温を表示できるようにした

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -3,19 +3,56 @@ import { displayWeatherPattern } from "./components/DisplayWeatherPattern";
 import { fetchWeatherApi } from "openmeteo";
 
 export const Weather = () => {
-  const [weatherData, setWeatherData] = useState<any>(null);
+  const [weatherData, setWeatherData] = useState<{
+    daily: any;
+    current_weather: { weathercode: number };
+  }>({ daily: {}, current_weather: { weathercode: 0 } });
   const [currentWeather, setCurrentWeather] = useState<string>("");
 
   const fetchWeatherData = async (latitude: number, longitude: number) => {
     console.log(latitude, longitude);
-    await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setWeatherData(data);
-      });
+    const url = "https://api.open-meteo.com/v1/forecast";
+    const params = {
+      latitude: latitude,
+      longitude: longitude,
+      daily: [
+        "weather_code",
+        "temperature_2m_max",
+        "temperature_2m_min",
+        "precipitation_probability_max",
+        "wind_speed_10m_max",
+      ],
+    };
+    const responses = await fetchWeatherApi(url, params);
+
+    const range = (start: number, stop: number, step: number) =>
+      Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+    const response = responses[0];
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+    const daily = response.daily();
+
+    const fetchWeatherApiResult = {
+      daily: {
+        time: daily
+          ? range(
+              Number(daily.time()),
+              Number(daily.timeEnd()),
+              daily.interval()
+            ).map((t) => new Date((t + utcOffsetSeconds) * 1000))
+          : [],
+        weatherCode: daily?.variables(0)?.valuesArray() ?? [],
+        temperature2mMax: daily?.variables(1)?.valuesArray() ?? [],
+        temperature2mMin: daily?.variables(2)?.valuesArray() ?? [],
+        precipitationProbabilityMax: daily?.variables(3)?.valuesArray() ?? [],
+        windSpeed10mMax: daily?.variables(4)?.valuesArray() ?? [],
+      },
+    };
+
+    setWeatherData((prevData) => ({
+      ...prevData,
+      daily: fetchWeatherApiResult.daily,
+    }));
   };
 
   useEffect(() => {
@@ -37,6 +74,7 @@ export const Weather = () => {
   }, []);
 
   useEffect(() => {
+    console.log(weatherData);
     if (weatherData) {
       const currentWeather = displayWeatherPattern(weatherData);
       setCurrentWeather(currentWeather);
@@ -46,8 +84,20 @@ export const Weather = () => {
   return (
     <>
       <h2>Weather App</h2>
-      <p>現在の天気: {currentWeather}</p>
-      <p>気温: {weatherData?.current_weather.temperature}°C</p>
+      {weatherData &&
+        weatherData.daily &&
+        weatherData.daily.temperature2mMax &&
+        weatherData.daily.temperature2mMin && (
+          <>
+            <p>現在の天気: {currentWeather}</p>
+            <p>
+              最高気温: {weatherData.daily.temperature2mMax[0].toFixed(1)}°C
+            </p>
+            <p>
+              最低気温: {weatherData.daily.temperature2mMin[0].toFixed(1)}°C
+            </p>
+          </>
+        )}
     </>
   );
 };
