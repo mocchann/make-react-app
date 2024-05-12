95caddc1a3825b5cf529c31d66709b6637e490af
もっちゃん <s7056i3056@gmail.com>
2024-05-12 14:42:55 +0900

    refactor: 実態に近い命名に修正した

---
+++ weather/src/components/getWeatherFromCode.ts
@@ -0,0 +1,30 @@
+export const getWeatherFromCode = (weatherCode: number): string => {
+  switch (weatherCode) {
+    case 0:
+      return "晴天🌞";
+    case 1:
+    case 2:
+    case 3:
+      return "曇り☁️";
+    case 45:
+    case 61:
+    case 63:
+    case 65:
+    case 66:
+    case 67:
+      return "雨☂️";
+    case 80:
+    case 81:
+    case 82:
+      return "にわか雨☔️";
+    case 71:
+    case 73:
+    case 75:
+    case 77:
+    case 85:
+    case 86:
+      return "雪⚡️";
+    default:
+      return "自分で外見ろ⭐️";
+  }
+};
