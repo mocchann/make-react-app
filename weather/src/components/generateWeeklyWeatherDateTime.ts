2906c0ac84be911de55ee14c5eda8eb437fe674e
もっちゃん <s7056i3056@gmail.com>
2024-05-16 23:37:34 +0900

    refactor: 意図がわかりにくい実装にコメントを追加した
    また元々日本時間であったためutcOffsetSecondsを削除した

--- weather/src/components/generateWeeklyWeatherDateTime.ts
+++ weather/src/components/generateWeeklyWeatherDateTime.ts
@@ -2,16 +2,17 @@ import { generateWeekRange } from "./generateWeekRange";
 import { VariablesWithTime } from "@openmeteo/sdk/variables-with-time";
 
 export const generateWeeklyWeatherDateTime = (
-  weatherCondition: VariablesWithTime | null,
-  utcOffsetSeconds: number
+  weatherCondition: VariablesWithTime | null
 ): Date[] => {
   if (!weatherCondition) return [];
 
   const weekRange = generateWeekRange(
+    // NOTE: BigIntのためNumberに変換
     Number(weatherCondition.time()),
     Number(weatherCondition.timeEnd()),
     weatherCondition.interval()
   );
 
-  return weekRange.map((t) => new Date((t + utcOffsetSeconds) * 1000));
+  // Dateオブジェクトはミリ秒で扱うため、秒に変換して返す
+  return weekRange.map((t) => new Date(t * 1000));
 };
