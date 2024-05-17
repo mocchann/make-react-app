81e5d424fc1ad158db89945d9692d9f81b3102a0
もっちゃん <s7056i3056@gmail.com>
2024-05-18 00:02:51 +0900

    add: わかりにくい実装のため、コメントで例を書いた

--- weather/src/components/generateWeekRange.ts
+++ weather/src/components/generateWeekRange.ts
@@ -1,5 +1,7 @@
 /**
  * 年月日を生成するために、time(start)からtimeEnd(stop)までの間隔をinterval(step)に基づいたnumberの配列を返す
+ * ex. generateWeekRange(0, 10, 2) => [0, 2, 4, 6, 8]
+ * HACK: この実装直感的にわかりづらすぎるのでどうにかしたい
  */
 export const generateWeekRange = (
   start: number,
