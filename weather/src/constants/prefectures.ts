634bdabfde662da17b7edda2dd89428892eea5b4
もっちゃん <s7056i3056@gmail.com>
2024-05-18 17:27:42 +0900

    add: 正確な緯度・経度を追加した
    また、ページにどこの天気予報かを表示されるためにname属性を追加した

--- weather/src/constants/prefectures.ts
+++ weather/src/constants/prefectures.ts
@@ -1,5 +1,6 @@
 export const prefectures = {
   current: {
+    name: "現在地",
     latitude: 0,
     longitude: 0,
   },
@@ -10,32 +11,37 @@ export const prefectures = {
   },
   saitama: {
     name: "埼玉",
-    latitude: 35.861729,
-    longitude: 139.645482,
+    latitude: 35.87217444432357,
+    longitude: 139.64617819997227,
   },
   chiba: {
     name: "千葉",
-    latitude: 35.605056,
-    longitude: 140.123308,
+    latitude: 35.61483038837763,
+    longitude: 140.10706968983132,
   },
   kanagawa: {
     name: "神奈川",
-    latitude: 35.4477,
-    longitude: 139.6425,
+    latitude: 35.44232979503489,
+    longitude: 139.63416535999303,
   },
   nagano: {
     name: "長野",
-    latitude: 35.4437,
-    longitude: 139.638,
+    latitude: 36.16682434745377,
+    longitude: 138.04484227071526,
   },
   okayama: {
     name: "岡山",
-    latitude: 34.655,
-    longitude: 133.919,
+    latitude: 34.74817040284675,
+    longitude: 133.87316566956372,
   },
   fukuoka: {
     name: "福岡",
-    latitude: 33.606,
-    longitude: 130.418,
+    latitude: 33.66657854607365,
+    longitude: 130.38811163151152,
+  },
+  nagasaki: {
+    name: "長崎",
+    latitude: 32.82823877610394,
+    longitude: 129.8555640481687,
   },
 };
