15c18f4b9c7e695c3a5285399f77c71f85e0a454
もっちゃん <s7056i3056@gmail.com>
2024-05-19 11:19:41 +0900

    refactor: apiリクエストの実行と結果を返すだけの関数にした

--- weather/src/components/execApiRequest.ts
+++ weather/src/components/execApiRequest.ts
@@ -4,7 +4,7 @@ import { OPEN_API_URL } from "../constants/openApiUrl";
 import { generateWetherCondition } from "./generateWetherCondition";
 import { weatherDataType } from "../types/weatherDataType";
 
-export const execApiAndSetWeatherData = async (
+export const execApiRequest = async (
   latitude: number,
   longitude: number
 ): Promise<Pick<weatherDataType, "weatherCondition">> => {
