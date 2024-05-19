2a569979ed7e01e899af79a4cef06b1a175250e3
もっちゃん <s7056i3056@gmail.com>
2024-05-19 22:59:05 +0900

    refactor: 冗長な型を修正した

--- weather/src/functions/getWeatherForecastCurrentLocation.ts
+++ weather/src/functions/getWeatherForecastCurrentLocation.ts
@@ -1,18 +1,17 @@
 import { execApiRequest } from "../apis/execApiRequest";
 import { WeatherDataType } from "../types/WeatherDataType";
 
-export const getWeatherForecastCurrentLocation = async (): Promise<
-  WeatherDataType | Promise<void>
-> => {
-  try {
-    const position = await new Promise<GeolocationPosition>(
-      (resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      }
-    );
-    const { latitude, longitude } = position.coords;
-    return await execApiRequest(latitude, longitude);
-  } catch (error) {
-    console.error(error);
-  }
-};
+export const getWeatherForecastCurrentLocation =
+  async (): Promise<WeatherDataType | void> => {
+    try {
+      const position = await new Promise<GeolocationPosition>(
+        (resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(resolve, reject);
+        }
+      );
+      const { latitude, longitude } = position.coords;
+      return await execApiRequest(latitude, longitude);
+    } catch (error) {
+      console.error(error);
+    }
+  };
