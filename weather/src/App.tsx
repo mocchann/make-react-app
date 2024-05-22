d3fb04a50b6cdb4917455c3ef1e115c090cce7a5
もっちゃん <s7056i3056@gmail.com>
2024-05-22 23:33:58 +0900

    Revert "refactor: execute"
    This reverts commit cd098a856fb6548b5313439bab1d0a53baf130f8.

--- weather/src/App.tsx
+++ weather/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo, useCallback } from "react";
+import { useState, useEffect, useMemo } from "react";
 import { getWeatherForecastCurrentLocation } from "./functions/getWeatherForecastCurrentLocation";
 import { generatePrefectures } from "./helpers/generatePrefectures";
 import { PrefButton } from "./components/PrefButton";
@@ -23,50 +23,34 @@ export const Weather = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [displayLocation, setDisplayLocation] = useState<string>("");
 
-  const execute = useCallback(
-    async (latitude: number, longitude: number): Promise<void> => {
-      const dailyWeatherVariables = await getWeatherForecastCurrentLocation({
-        latitude,
-        longitude,
-      });
-
-      if (!dailyWeatherVariables) {
-        console.error("天気情報の取得に失敗");
-        return;
-      }
-
-      const result = formatWeatherVariables(dailyWeatherVariables);
-
-      if (
-        JSON.stringify(weatherData.weatherCondition) ===
-        JSON.stringify(result.weatherCondition)
-      ) {
-        return;
-      }
-
-      setDisplayLocation("現在地");
-      setWeatherData((beforeWeatherData) => ({
-        ...beforeWeatherData,
-        weatherCondition: result.weatherCondition,
-      }));
-      setIsLoading(false);
-    },
-    []
-  );
-
-  const getPosition = async () => {
-    const position = await new Promise<GeolocationPosition>(
-      (resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-      }
-    );
-    const { latitude, longitude } = position.coords;
-    execute(latitude, longitude);
-  };
-
   useEffect(() => {
-    getPosition();
-  }, [execute]);
+    execute();
+  }, []);
+
+  const execute = async (): Promise<void> => {
+    const dailyWeatherVariables = await getWeatherForecastCurrentLocation();
+
+    if (!dailyWeatherVariables) {
+      console.error("天気情報の取得に失敗");
+      return;
+    }
+
+    const result = formatWeatherVariables(dailyWeatherVariables);
+
+    if (
+      JSON.stringify(weatherData.weatherCondition) ===
+      JSON.stringify(result.weatherCondition)
+    ) {
+      return;
+    }
+
+    setDisplayLocation("現在地");
+    setWeatherData((beforeWeatherData) => ({
+      ...beforeWeatherData,
+      weatherCondition: result.weatherCondition,
+    }));
+    setIsLoading(false);
+  };
 
   const formattedWeatherData = useMemo(
     () => formatDisplayWeatherData(weatherData),
@@ -78,7 +62,7 @@ export const Weather = () => {
   return (
     <>
       <h1>Weather App</h1>
-      <button onClick={getPosition}>現在地</button>
+      <button onClick={execute}>現在地</button>
       {prefArray.map((locationKey, i) => (
         <PrefButton
           key={i}
